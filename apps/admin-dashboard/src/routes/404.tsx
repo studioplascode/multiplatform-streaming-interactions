d84068c617337308dc05d633b5c16f18198882e2
AriGamesLP <the.bros.85.03@gmail.com>
2023-04-16 19:48:00 +0200

    404 colors

--- apps/admin-dashboard/src/routes/404.tsx
+++ apps/admin-dashboard/src/routes/404.tsx
@@ -1,23 +1,23 @@
 const Page404 = () => {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center bg-zinc-950 px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          <p className="text-base font-semibold text-zinc-300">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-zinc-100 sm:text-5xl">
             Page not found
           </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
+          <p className="mt-6 text-base leading-7 text-white">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="/dashboard"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md mpsi-gradient px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
             </a>
-            <a href="https://discord.gg/B6NJxzEXFn" className="text-sm font-semibold text-gray-900">
+            <a href="https://discord.gg/B6NJxzEXFn" className="text-sm font-semibold text-white">
               Get Help on Discord<span aria-hidden="true">&rarr;</span>
             </a>
           </div>
